e8c2b48bac3b7bc3d072d34f06d7b2d8bcc69c6e
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-03-01 15:19:34 -0500

    Added a test to make sure employee was in the database could be called.

--- test_employee.rb
+++ test_employee.rb
@@ -3,19 +3,32 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'minitest/focus'
 require 'bundler/setup'
+require "active_record"
+require 'pry'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "employees.db"
+  )
 
 class EmployeeTest < Minitest::Test
 
-  def robby
-    @_robby ||= Employee.new(name: "Robby Dore",
-                                email_address: "Rdore88@gmail.com",
-                                phone_number: "404-277-3952",
-                                salary: 60000
-                              )
-  end
+# Employee.new(name: "Robby Dore",
+#               email_address: "Rdore88@gmail.com",
+#               phone_number: "404-277-3952",
+#               salary: 60000
+#             ).save
+
+
 
   def test_employee_exists
     assert Employee
   end
+
+  def test_employee
+    assert_equal "Robby Dore", Employee.first.name
+  end
+
+  p Employee
 
 end
