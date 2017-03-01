b99f25c04af9b73e7c20d0a0d663259f767d86a2
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-03-01 16:24:40 -0500

    Added function to save review.

--- test_employee.rb
+++ test_employee.rb
@@ -5,11 +5,7 @@ require 'minitest/focus'
 require 'bundler/setup'
 require "active_record"
 require 'pry'
-
-ActiveRecord::Base.establish_connection(
-  adapter: "sqlite3",
-  database: "employees.db"
-  )
+require_relative 'db_connection'
 
 class EmployeeTest < Minitest::Test
 
@@ -19,8 +15,6 @@ class EmployeeTest < Minitest::Test
 #               salary: 60000
 #             ).save
 
-
-
   def test_employee_exists
     assert Employee
   end
@@ -29,6 +23,16 @@ class EmployeeTest < Minitest::Test
     assert_equal "Robby Dore", Employee.first.name
   end
 
-  p Employee
+  def test_change_salary
+    skip
+    Employee.first.change_salary(5000)
+    assert_equal 65000, Employee.first.salary
+  end
+
+  def test_leave_review
+    Employee.first.leave_review("Robby is a great worker")
+    assert_equal "Robby is a great worker", Employee.first.review
+  end
+
 
 end
