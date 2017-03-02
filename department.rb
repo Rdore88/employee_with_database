aa88dc7783775d475413ab6a5b42880d5b731915
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-03-02 05:58:42 -0500

    Created ability to add a department id to an employee.

--- department.rb
+++ department.rb
@@ -1,6 +1,16 @@
 require "active_record"
-require_relative 'db_connection_departments'
+require_relative 'employee'
+require_relative 'db_connection'
 
 class Department < ActiveRecord::Base
 
+  def add_employee(employee)
+    Employee.first.department_id=Department.first.id
+    Employee.first.save
+  end
+
+  def total_department_payroll(employees)
+    Department.where(self.id = employees.department_id)
+
+  end
 end
--- test_department.rb
+++ test_department.rb
@@ -4,8 +4,9 @@ require 'minitest/pride'
 require 'minitest/focus'
 require 'bundler/setup'
 require "active_record"
+require_relative 'employee'
 require 'pry'
-require_relative 'db_connection_departments'
+require_relative 'db_connection'
 
 class DepartmentTest < Minitest::Test
 
@@ -18,6 +19,17 @@ class DepartmentTest < Minitest::Test
 
   def test_department_name
     assert_equal "Software", Department.first.name
+  end
+
+  def test_adding_employee
+    Department.first.add_employee(Employee.first)
+    assert_equal Department.first.id,Employee.first.department_id
+  end
+
+  def test_total_department_payroll
+    skip
+    assert_equal 65000, Department.first.total_department_payroll(Employee.all)
+
   end
 
 end
